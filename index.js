f5e237422111f5f63792c58469d549460d4fd38c
tnsherpa1 <tnsherpa1@gmail.com>
2018-02-10 10:33:52 -0800

    defines schema for artist, genre and films model

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = proces
 const sequelize = new Sequelize('database', '', '', {
   host: 'localhost',
   dialect: 'sqlite',
-
+  define: {
+    timestamps: false //Fixes 'cannot get created at column error'
+  },
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
+    idle: 10000,
   },
-
   // SQLite only
   storage: DB_PATH
 });
@@ -29,6 +30,71 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+
+  // Define schema for models
+  //Film schema
+  const Film = sequelize.define('films', {
+  title: {
+    type: Sequelize.STRING
+  },
+  release_date: {
+    type: Sequelize.STRING
+  },
+  tagline: {
+    type: Sequelize.STRING
+  },
+  revenue: {
+    type: Sequelize.INTEGER
+  },
+  budget: {
+    type: Sequelize.INTEGER
+  },
+  runtime: {
+    type: Sequelize.INTEGER
+  },
+  original_language: {
+    type: Sequelize.STRING
+  },
+  status: {
+    type: Sequelize.STRING
+  },
+  genre_id: {
+    type: Sequelize.STRING
+  }
+});
+//Artist schema
+const Artist = sequelize.define('artists', {
+  name: {
+    type: Sequelize.STRING
+  },
+  birthday: {
+    type: Sequelize.STRING
+  },
+  deathday: {
+    type: Sequelize.STRING
+  },
+  gender: {
+      type: Sequelize.INTEGER
+  },
+  place_of_birth: {
+    type: Sequelize.STRING
+  }
+});
+  //Genre schema
+  const Genre = sequelize.define('genres', {
+    name: {
+      type: Sequelize.STRING
+    }
+  }
+);
+
+  //Check by get
+  app.get('/', function(req,res) {
+    Film.findAll({}).then((results) => {
+      res.json(results.length);
+    });
+  });
+
 
 
 
