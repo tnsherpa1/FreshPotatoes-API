52285fe4da8d422166baf8e37054e55caec1516f
tnsherpa1 <tnsherpa1@gmail.com>
2018-02-10 10:13:18 -0800

    Establish database connection using sequelize ORM

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ const sqlite = require('sqlite'),
       app = express();
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+// Establish connection
+const sequelize = new Sequelize('database', '', '', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: DB_PATH
+});
+//Check connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+
 
 // START SERVER
 Promise.resolve()
